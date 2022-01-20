fc36508cf407b4b8ce872de6fc9d9dae68ba7c63
Naiimtj <95881391+Naiimtj@users.noreply.github.com>
2022-01-20 14:07:09 +0100

    Primer commit

---
+++ src/data/monedas.js
@@ -0,0 +1,8 @@
+const monedas = [
+  { id: "EUR", nombre: "Euro" },
+  { id: "USD", nombre: "Dolar de Estados Unidos" },
+  { id: "MXN", nombre: "Peso Mexicano" },
+  { id: "GBP", nombre: "Libra Esterlina" },
+];
+
+export { monedas };
